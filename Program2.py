c7a173e4c9283b0e78d9b3d5333c3a40588b266e
BushyBrowsDEVS <BushyBrows17@outlook.com>
2024-09-04 15:21:26 +0000

    Finished assignment

--- Program2.py
+++ Program2.py
@@ -62,9 +62,9 @@ print(name)
 # - Prints it out twice on a single line so that there is an exclamation mark at the beginning of the line, 
 # - another between the two names and a third one at the end of the line.
 
-name = input ("Hey there, whats your name?\n")
+secondname = input ("Hey there, whats your name?\n")
 
-print("!"+name"!"+name"!")
+print("!"+secondname+"!"+secondname+"!")
 
 
 ## Problem 3 ##
@@ -77,11 +77,29 @@ print("!"+name"!"+name"!")
 #   - Street address: 91 Station Road
 #   - City and postal code: Folsom CA, 95630
 
+firstname = input("Hey there, what is your first name?\n")
+lastname= input("Your Last name?\n")
+address = input("What is your address?\n")
+city = input("What City do you live in?\n")
+state = input("What State do you live in?\n")
+postal = input("What is your postal code?\n")
+
+print("First Name: "+firstname)
+print("Last Name: "+lastname)
+print("Street Address: "+address)
+print("City and Postal Code: "+city+" "+state+", "+postal)
+
+
 ## Problem 4 ##
 #Please write a script that: 
 # - Asks for 3 words 
 # - Puts the words together with dashes and prints that out
 
+word1 = input("Give me a word.\n")
+word2 = input("Give me another word.\n")
+word3 = input("Give me one last word.\n")
+
+print(word1+"-"+word2+"-"+word3)
 
 ## Problem 5 ##
 #Please write a script that: 
@@ -94,3 +112,10 @@ print("!"+name"!"+name"!")
 #Mary is a valiant knight, born in the year 1572. 
 #One morning Mary woke up to an awful racket: a dragon was approaching the village. 
 #Only Mary could save the village's residents.
+
+storyname = input("What should the main characters name be?\n")
+year = input("What year should the story take place in?\n")
+
+print("Once apon a time, in "+year+", "+storyname+" the most skillful swordsperson in the land was fighting of nasty creatures.")
+print("All of the sudden "+storyname+" was leaped on from behind by a monster.")
+print(storyname+" could not fight the monster off, and "+storyname+" was sadly killed that day.")
